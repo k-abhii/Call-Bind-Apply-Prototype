436373e14fc0be8ca56b3f92fd6b7f175d926dba
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2022-09-14 14:41:13 +0530

    let block scope

--- var_let_const.js
+++ var_let_const.js
@@ -11,3 +11,25 @@ function fn (){
     console.log("Line 11 ->a->",a);
 }
 fn();
+/// chahe aap varible redeclare kar do but ek function me wo ek hi bna rahta h -> var ke case me 
+// let and const are block scope 
+// block-> space between two curly  braces is known as block {}
+
+
+//Ctrl+Shift+P to open your command palette and choose Select Emoji.
+// Search your emoji and click the emoji you want to use then Ctrl+V to paste it where you want
+let fruits ="apple";
+{
+    { 
+        let fruits ="orange";
+        {
+            {
+               console.log(fruits);
+            }
+            console.log(fruits);
+        }
+        console.log(fruits);
+    }
+    console.log(fruits);
+}
+console.log(fruits);
