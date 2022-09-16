57ea20b9e180c8bdf33cd06b05dfa8f440a67605
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2022-09-16 15:38:01 +0530

    PRIMITIVE DATA TYPES ARE UNMUTABLE IN MEMORY

--- playwithdatatypes.js
+++ playwithdatatypes.js
@@ -1,3 +1,8 @@
 var arr =[1,2,3,4,5,];
 arr[0]=7;
 console.log(arr); /// [ 7, 2, 3, 4, 5 ]
+
+// NON PRIMITIVE DATA TYPES VALUE DOES NOT CHANGE IN MEMORY 
+var name ="masai";
+name[0] ="M";
+console.log(name); ///  [ 7, 2, 3, 4, 5 ] // masai
