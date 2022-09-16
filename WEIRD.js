802269e87255896da50961224200400c618e5393
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2022-09-16 15:17:46 +0530

    JS

---
+++ WEIRD.js
@@ -0,0 +1,2 @@
+console.log(typeof null);
+console.log(1 == '1');
--- var_let_const.js
+++ var_let_const.js
@@ -1,16 +1,16 @@
 //var is function scope 
-function fn (){
-    var a=10;
-    console.log("line 4->a ->",a);
-    a++;
-    if(true){
-        console.log("line 7->a ->",a);
-        var a =30;
-        console.log("line 9 ->a->",a);
-    }
-    console.log("Line 11 ->a->",a);
-}
-fn();
+// function fn (){
+//     var a=10;
+//     console.log("line 4->a ->",a);
+//     a++;
+//     if(true){
+//         console.log("line 7->a ->",a);
+//         var a =30;
+//         console.log("line 9 ->a->",a);
+//     }
+//     console.log("Line 11 ->a->",a);
+// }
+// fn();
 /// chahe aap varible redeclare kar do but ek function me wo ek hi bna rahta h -> var ke case me 
 // let and const are block scope 
 // block-> space between two curly  braces is known as block {}
@@ -18,18 +18,33 @@ fn();
 
 //Ctrl+Shift+P to open your command palette and choose Select Emoji.
 // Search your emoji and click the emoji you want to use then Ctrl+V to paste it where you want
-let fruits ="apple";
-{
-    { 
-        let fruits ="orange";
-        {
-            {
-               console.log(fruits);
-            }
-            console.log(fruits);
-        }
-        console.log(fruits);
-    }
-    console.log(fruits);
+// let fruits ="apple";
+// {
+//     { 
+//         let fruits ="orange";
+//         {
+//             {
+//                console.log(fruits);
+//             }
+//             console.log(fruits);
+//         }
+//         console.log(fruits);
+//     }
+//     console.log(fruits);
+// }
+// console.log(fruits);
+
+// Example 2 
+function fn(){
+    let a =10;
+    console.log("Line Number 40->a",a);
+    a++;
+
+    if(true){
+       // console.log("Line Number 44 ->a->",a);  // error  ReferenceError: Cannot access 'a' before initialization
+        let a =30;
+        console.log("Line Number 46->a->",a);
 }
-console.log(fruits);
+console.log("Line Number 48->a->",a);
+}
+fn();
